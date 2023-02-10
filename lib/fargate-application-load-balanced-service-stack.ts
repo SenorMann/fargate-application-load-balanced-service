3828583bac6d8fbafe4049ea3acf74fe45ff9d09
Malik Mahmud <malikmahmud@safeguardworld.com>
2023-02-10 16:34:02 +0100

    define stack

--- lib/fargate-application-load-balanced-service-stack.ts
+++ lib/fargate-application-load-balanced-service-stack.ts
@@ -1,16 +1,25 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import ec2 = require('aws-cdk-lib/aws-ec2');
+import ecs = require('aws-cdk-lib/aws-ecs');
+import ecs_patterns = require('aws-cdk-lib/aws-ecs-patterns');
+import cdk = require('aws-cdk-lib');
 
 export class FargateApplicationLoadBalancedServiceStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create VPC and Fargate Cluster
+    // NOTE: Limit AZs to avoid reaching resource quotas
+    const vpc = ec2.Vpc.fromLookup(this, "MyVpc", {
+      vpcId: "vpc-04514834406cec305",
+    })
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FargateApplicationLoadBalancedServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Instantiate Fargate Service with just cluster and image
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "FargateService", {
+      cluster,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      },
+    });
   }
 }
